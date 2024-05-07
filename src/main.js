f11f30f717ef6016807f7204e2ea65ab72a956d5
judeallanhill <judeallanhill@icloud.com>
2024-05-07 07:16:12 -0700

    Adding the offical check drone status

--- src/main.js
+++ src/main.js
@@ -10,3 +10,26 @@ startBut.addEventListener("click", function() {
 stopBut.addEventListener("click", function() {
   console.log("We had a click, Stop");
 });
+function checkDroneStatus() {
+  fetch('http://127.0.0.1:3000/DroneOnline')
+  .then(response => {
+      if (!response.ok) {
+          throw new Error('Network response was not ok');
+      }
+      return response.json();
+  })
+  .then(data => {
+      console.log('Drone status:', data);
+      droneConnected = data
+      document.getElementById("connection").innerHTML = "Connected to the drone " + droneConnected
+
+  })
+  .catch(error => {
+      console.error('There was a problem checking drone status:', error);
+  });
+  setTimeout(() => {
+    checkDroneStatus()
+  }, 1000); // 6000 milliseconds = 6 seconds
+}
+
+checkDroneStatus();
