9cf0f64fa0a36900894014734e00fa7d78935448
judeallanhill <judeallanhill@icloud.com>
2024-06-22 22:43:41 -0700

    Updating webvieew

--- main.js
+++ main.js
@@ -53,7 +53,9 @@ function getGPSPoints() {
 
       // Add new points to the map
       GPSPoints.forEach(point => {
-        console.log(point.heading)
+        for(let i = 0; i < point.lenth; i++) {
+          console.log(point[i].heading)
+        }
         drawPoint(parseFloat(point.latitude), parseFloat(point.longitude), `Point at ${point.latitude}, ${point.longitude}`);
       });
     })
